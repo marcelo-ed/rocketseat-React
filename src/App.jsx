351945d579565bcf4fcfd28a987ed41f8284df2d
LIDK <iotarjr@live.com>
2023-05-31 15:55:27 -0300

    Início do projeto.

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Header }from './components/header.jsx'
+import './styles/global.css'
+
+
+function App() {
+  
+
+  return (
+    <>
+        <Header />
+
+        <div className='Container'>
+          <aside>
+              Sidebar
+          </aside>
+
+          <main>
+            
+          </main>
+        </div>
+    </>
+  )
+}
+
+export default App
