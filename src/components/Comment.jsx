22923dc4e1d860781ffcd664205494bd7491a273
marcelo-ed <marceledu2@gmail.com>
2023-06-14 15:59:39 -0300

    componente Comment finalizado

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,11 @@
-import styles from 'Comment.module.css'
+import styles from './Comment.module.css'
+import { ThumbsUp, Trash } from 'phosphor-react'
 
 export function Comment(){
     return(
 
 
-        <div className={styles.Comment}>
+        <div className={styles.comment}>
             
         <div className={styles.commentBox}>
 
@@ -14,15 +15,20 @@ export function Comment(){
                         <strong>marcelo-ed</strong>
                         <time title='12 de julho de 2023, às 15:58' dateTime='12-06-2023 15:58'> Há 1 hora </time>
                     </div>
+
+                    <button>
+                        <Trash title="deleter comentário" size={24} />
+                    </button>
                 </header>
 
                 <p>Seja estranho. Seja aleatório. Seja quem você é. Porque você nunca sabe quem amaria a pessoa que você esconde.</p>
             </div>
+            
+        <footer> <button><ThumbsUp />  likes <span>20</span></button></footer>
 
         </div>
 
 
-        <footer>Footer</footer>
         </div>
     )
 }
